a2eab444d310cdd486bbfdef56d5befc8b3f0117
Joshua Wootonn <jose56wonton@gmail.com>
2017-08-12 17:31:12 -0500

    Finished Cover's before putting bootstrap and stuff in

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 function slider() {
     
-    if (document.body.scrollTop > $("div.nav-1").height()) {    
-        $('.nav-2').stop().animate({"margin-top": '0'});
+    if (document.body.scrollTop > $("div.cover-1").height()) {    
+        $('.nav').stop().animate({"margin-top": '0'});
     }
     else
-        $('.nav-2').stop().animate({"margin-top": '-60px'}); 
+        $('.nav').stop().animate({"margin-top": '-60px'}); 
 }
 $(window).scroll(function () {
     slider();
