99009bdac9a5fa9f5a8a7c0c4927f2aaabae8c20
Joshua Wootonn <jose56wonton@gmail.com>
2017-08-13 16:38:42 -0500

    Updated README.md, Deleted unnecessary files, Condesced code. Fixed file extensions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# event-barricade-solutions
-Freelance website for EBS(Event Barricade Solutions)
+# Event Barricade Solutions
+This is my first freelance job. I was created for a company called Event Barricade Solutions
+
+## Demo
+It can be found at this [url](http://joshuawootonn.com/event-barricade-solutions/)
+
+## New Stuff
+Some technologies I used extensively for the first time were Sass, Parallax.js, and Bootstrap.
+
+## Challenges
+Creating this website was difficult because it was the first time in my WebDev journey that I 
+had to be creative. Overall though I think it is a nice looking website, and will do it's job 
+well.
+
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// This is the functionally that I use to get the fixed header to come 
+// after the first cover photo.
 function slider() {
     
     if (document.body.scrollTop > $("div.cover-1").height()) {    
@@ -12,4 +14,72 @@ $(window).scroll(function () {
 
 $(document).ready(function () {
     //slider();
+});
+// This is the code I used for the slide in functionality on a lot of the text.
+// As you can see it is not of my creation; however, it is not a copy and paste
+// I did edit about 50% of the code.
+(function($) {
+
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
+
+var win = $(window);
+
+var allMods1 = $(".row");
+var allMods2 = $(".parallax-window .text-wrapper h1");
+
+allMods1.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+allMods2.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  allMods1.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+        el.addClass("come-in"); 
+    }
+  });
+
+  allMods2.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+        el.addClass("come-in"); 
+    }
+  });
+  
 });
